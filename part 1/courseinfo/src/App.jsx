73a6843b36fce14a3d6d0f6498fb7760967ca3fa
LewPort <porter.lewis@gmail.com>
2025-08-07 01:45:19 +0100

    refactor: change app variables to array of objects

--- part 1/courseinfo/src/App.jsx
+++ part 1/courseinfo/src/App.jsx
@@ -39,18 +39,27 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
   return (
     <div>
       <Header title = {course}/>
-      <Content part1={part1} val1={exercises1} part2={part2} val2={exercises2} part3={part3} val3={exercises3}/>
-      <Total totalExercises={exercises1 + exercises2 + exercises3} />
+      <Content part1={parts[0].name} val1={parts[0].exercises} 
+      part2={parts[1].name} val2={parts[1].exercises} 
+      part3={parts[2].name} val3={parts[2].exercises}/>
+      <Total totalExercises={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
     </div>
   )
 }
